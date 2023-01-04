724c2614b7deea5e566222e8a61c914b54f4a95f
ClaytonDewey <clay@dryadmedia.com>
2023-01-04 12:28:28 -0600

    Add bottom margin

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 const SearchBarStyles = styled.div`
   position: relative;
   width: 100%;
+  margin-bottom: 2rem;
   input {
     background-color: var(--clr-dark-blue);
     border-radius: var(--bdr-rds);
