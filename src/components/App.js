9b607599da3bbf42026ea47aec209b1009e47b20
Clayton Dewey <clay@dryadmedia.com>
2023-01-03 18:46:22 -0600

    Reorganize file structure
    Setup base GlobalStyles

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+const App = () => {
+  return (
+    <div>
+      <h1>devfinder</h1>
+
+      <p>Light</p>
+      <p>Dark</p>
+
+      <p>Search GitHub username...</p>
+      <p>Search</p>
+
+      <p>Joined</p>
+
+      <p>Repos</p>
+      <p>Followers</p>
+      <p>Following</p>
+    </div>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,61 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createGlobalStyle } from "styled-components";
+import App from "./components/App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const GlobalStyles = createGlobalStyle`
+  :root {
+    --clr-dark: #141D2F;
+    --clr-light: #f6f8ff;
+
+    --ff-mono: "Space Mono";
+    --fw-bold: 700;
+    --fw-reg: 400;
+    --fs-xlg: 2.8rem;
+    --fs-lg: 2.2rem;
+    --fs-md: 1.6rem;
+    --fs-sm: 1.5rem;
+    --fs-xsm: 1.3rem;
+  }
+  html {
+    background: var(--clr-dark);
+    color: var(--clr-light);
+    font-family: var(--ff-mono), -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-weight: var(--ff-reg);
+    font-size: 62.5%;
+  }
+  body {
+    font-size: var(--fs-sm);
+    line-height: 2.5rem;
+    font-weight: var(--fw-reg);
+  }
+  h1 {
+    font-size: var(--fs-xlg);
+    line-height: 3.8rem;
+  }
+  h2 {
+    font-size: var(--fs-lg);
+    line-height: 3.3rem;
+  }
+  h3 {
+    font-size: var(--fs-md);
+  }
+  h4 {
+    font-size: var(--fs-xsm);
+  }
+  h1, h2 {
+    font-weight: var(--fw-bold);
+  }
+  h3, h4 {
+    font-weight: var(--fw-reg);
+  }
+`;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <GlobalStyles />
     <App />
   </React.StrictMode>
 );
