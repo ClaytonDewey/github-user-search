7c5da64f239323539d56b70d5739041ddc570d0a
ClaytonDewey <clay@dryadmedia.com>
2023-01-10 10:19:05 -0600

    Import Header and wrap items

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,15 +1,26 @@
 import styled from 'styled-components';
+import { GridStyles } from '..';
 
-const UserInfoStyles = styled.div``;
+const UserInfoStyles = styled.div`
+  margin-bottom: 3rem;
+  a {
+    display: inline-block;
+    margin-bottom: 3rem;
+  }
+`;
 
 const UserInfo = ({ user, joinedDate }) => {
   return (
     <UserInfoStyles>
-      <h2>{user.name}</h2>
-      <a href={user.html_url} target='_blank' rel='noreferrer'>
-        @{user.login}
-      </a>
-      <p>Joined {joinedDate}</p>
+      <GridStyles>
+        <div>
+          <h2>{user.name}</h2>
+          <a href={user.html_url} target='_blank' rel='noreferrer'>
+            @{user.login}
+          </a>
+        </div>
+        <p>Joined {joinedDate}</p>
+      </GridStyles>
       {!user.bio ? <p>No user bio</p> : <p>{user.bio}</p>}
     </UserInfoStyles>
   );
